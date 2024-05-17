abc8332a26691a6b0dcc3ea5b410ac5a4ef579e4
patrick-204 <p.ohara921@gmail.com>
2024-05-17 01:43:03 -0400

    Refactor functions in database.js to use the legitimate database.

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -19,14 +19,15 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool
+  .query(`SELECT * FROM users WHERE email = $1`, [email])
+  .then((result) => {
+    console.log(result.rows[0]);
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
 };
 
 /**
@@ -35,7 +36,15 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(`SELECT * FROM users WHERE id = $1`, [id])
+  .then((result) => {
+    console.log(result.rows[0]);
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
 };
 
 /**
@@ -44,10 +53,16 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const {name, email, password} = user;
+
+  return pool
+  .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`, [name, email, password])
+  .then((result) => {
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
 };
 
 /// Reservations
@@ -77,7 +92,7 @@ const getAllProperties = (options, limit = 10) => {
       return result.rows;
     })
     .catch((err) => {
-      console.log(err.message);
+      console.error(err.message);
     });
 };
 
